e41feaca72e4a0f18508307988301844c2f69610
Steve Shi <xuanlian@mac.com>
2026-05-29 14:48:23 +0800

    Fix MacSSH download URL name in cask

--- Casks/macssh.rb
+++ Casks/macssh.rb
@@ -2,7 +2,7 @@ cask "macssh" do
   version "1.7.6"
   sha256 "581c013d34deee88090d3a906a4b7bbf0cfdb26c3cd89dc1cb1cbafa32e5b50d"
 
-  url "https://github.com/SteveShi/MacSSH/releases/download/v#{version}/MacSSH-AppleSilicon.dmg"
+  url "https://github.com/SteveShi/MacSSH/releases/download/v#{version}/MacSSH.dmg"
   name "MacSSH"
   desc "A modern SSH & SFTP client for macOS powered by the Ghostty terminal engine"
   homepage "https://github.com/SteveShi/MacSSH"
